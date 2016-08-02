82ebae9e2d6f985637591fe43b48e62f56f429fb
congdonglangsoi <nguyenvandinh.khmt2@gmail.com>
2016-08-02 09:35:14 +0700

    update page 404

--- 404.php
+++ 404.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
-
+<div class="page-content-error white">
+    <p class="center blue-text font-size-16 blemil-error-text"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i><strong>Đã có lỗi xảy ra</strong></p>
+    <p class="center">Bạn có thể truy cập vào
+        <a class="black-text link-home-error" href="<?php echo bloginfo('home');?> ">trang chủ </a>
+        hoặc sử dụng công cụ
+        <a href="#modal_search" class="modal-trigger black-text link-home-error" >tìm kiếm</a></p>
+</div>
 <?php get_footer(); ?>
